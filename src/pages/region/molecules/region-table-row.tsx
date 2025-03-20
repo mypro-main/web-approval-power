b9080d33be9a06ed96488f0dd17f9461fc637d98
arrdix <mail.ardix@gmail.com>
2025-03-20 15:01:44 +0700

    feat(region): implement region

--- src/pages/region/molecules/region-table-row.tsx
+++ src/pages/region/molecules/region-table-row.tsx
@@ -1,17 +1,8 @@
 import capitalize from '@mui/utils/capitalize';
 import TableRow from '@mui/material/TableRow';
 import TableCell from '@mui/material/TableCell';
-import IconButton from '@mui/material/IconButton';
-import Iconify from '../../../components/iconify';
-import { RegionQuickEditForm } from './region-quick-edit-form';
-import { useBoolean } from '../../../hooks/use-boolean';
 import Label from '../../../components/label';
 import { IRegionItem } from '../../../types/region';
-import { Button } from '@mui/material';
-import { ConfirmDialog } from '../../../components/custom-dialog';
-import { useCallback } from 'react';
-import CustomPopover, { usePopover } from '../../../components/custom-popover';
-import MenuItem from '@mui/material/MenuItem';
 
 type Props = {
   selected: boolean;
@@ -20,15 +11,6 @@ type Props = {
 
 export default function RegionTableRow({ row, selected }: Props) {
   const { id, name, description, status } = row;
-
-  const popover = usePopover();
-
-  const confirm = useBoolean();
-  const quickEdit = useBoolean();
-
-  const onDeleteRow = useCallback(() => {
-    console.log('deleted');
-  }, []);
 
   return (
     <>
@@ -48,52 +30,7 @@ export default function RegionTableRow({ row, selected }: Props) {
             {status}
           </Label>
         </TableCell>
-        <IconButton color={popover.open ? 'inherit' : 'default'} onClick={popover.onOpen}>
-          <Iconify icon="eva:more-vertical-fill" />
-        </IconButton>
       </TableRow>
-
-      <CustomPopover
-        open={popover.open}
-        onClose={popover.onClose}
-        arrow="right-top"
-        sx={{ width: 140 }}
-      >
-        <MenuItem
-          onClick={() => {
-            quickEdit.onTrue();
-            popover.onClose();
-          }}
-        >
-          <Iconify icon="solar:pen-bold" />
-          Edit
-        </MenuItem>
-
-        <MenuItem
-          onClick={() => {
-            confirm.onTrue();
-            popover.onClose();
-          }}
-          sx={{ color: 'error.main' }}
-        >
-          <Iconify sx={{ color: 'error' }} icon="solar:trash-bin-minimalistic-bold" />
-          Delete
-        </MenuItem>
-      </CustomPopover>
-
-      <RegionQuickEditForm currentRegion={row} open={quickEdit.value} onClose={quickEdit.onFalse} />
-
-      <ConfirmDialog
-        open={confirm.value}
-        onClose={confirm.onFalse}
-        title="Delete"
-        content="Are you sure want to delete?"
-        action={
-          <Button variant="contained" color="error" onClick={onDeleteRow}>
-            Delete
-          </Button>
-        }
-      />
     </>
   );
 }
