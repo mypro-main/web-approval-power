0d6d4cfe2b90bf891d6d75eead57aae8d4021d17
Yudistira Ardi <44466280+arrdix@users.noreply.github.com>
2025-03-28 14:19:40 +0700

    feat(user): implement edit user

--- src/pages/user/molecules/user-quick-edit-form.tsx
+++ src/pages/user/molecules/user-quick-edit-form.tsx
@@ -15,9 +15,6 @@ import { useGetUserStatuses } from '../../../services/user/hooks/use-get-user-st
 import { useGetUserRoles } from '../../../services/user/hooks/use-get-user-roles';
 import Label from '../../../components/label';
 import { useUpdateUser } from '../../../services/user/hooks/use-update-user';
-import { RegionService } from '../../../services/region/region-service';
-import { TerritoryService } from '../../../services/territory/territory-service';
-import { RHFAutocompleteAsyncOnSearch } from '../../../components/hook-form/rhf-autocomplete';
 import { IUserItem } from '../../../types/user';
 
 type Props = {
@@ -27,138 +24,109 @@ type Props = {
 };
 
 export default function UserQuickEditForm({ currentUser, open, onClose }: Props) {
-  // const { mutateUser, error } = useUpdateUser();
-  // const { statuses } = useGetUserStatuses();
-  // const { roles } = useGetUserRoles();
-  //
-  // const regionService = new RegionService();
-  // const territoryService = new TerritoryService();
-  //
-  // const getRegion = (keyword?: string) => regionService.getAll({ name: keyword });
-  // const getTerritory = (keyword?: string) => territoryService.getAll({ name: keyword });
-  //
-  // const defaultValues = useMemo(
-  //   () => ({
-  //     name: currentUser.name,
-  //     email: currentUser.email,
-  //     role: currentUser.role,
-  //     status: currentUser.status,
-  //     regionIds: currentUser.UserRegion.map((region) => region.Region.id),
-  //     territoryIds: currentUser.UserTerritory.map((territory) => territory.Territory.id),
-  //   }),
-  //   [currentUser]
-  // );
-  //
-  // const methods = useForm({
-  //   resolver: yupResolver(EditUserSchema),
-  //   defaultValues,
-  // });
-  //
-  // const {
-  //   handleSubmit,
-  //   setError,
-  //   formState: { isSubmitting },
-  // } = methods;
-  //
-  // useEffect(() => {
-  //   if (error) {
-  //     setError('email', error);
-  //   }
-  // }, [error]);
-  //
-  // const onSubmit = handleSubmit(async (payload) => {
-  //   const { id } = currentUser;
-  //   await mutateUser({ id, payload });
-  // });
+  const { mutateUser, error } = useUpdateUser();
+  const { statuses } = useGetUserStatuses();
+  const { roles } = useGetUserRoles();
+
+  const defaultValues = useMemo(
+    () => ({
+      name: currentUser.name,
+      email: currentUser.email,
+      role: currentUser.role,
+      status: currentUser.status,
+      jobTitle: currentUser.jobTitle ?? '',
+    }),
+    [currentUser]
+  );
+
+  const methods = useForm({
+    resolver: yupResolver(EditUserSchema),
+    defaultValues,
+  });
+
+  const {
+    handleSubmit,
+    setError,
+    formState: { isSubmitting },
+  } = methods;
+
+  useEffect(() => {
+    if (error) {
+      setError('email', error);
+    }
+  }, [error]);
+
+  const onSubmit = handleSubmit(async (payload) => {
+    const { id } = currentUser;
+    await mutateUser({ id, payload });
+  });
 
   return (
-    <></>
-    // <Dialog
-    //   fullWidth
-    //   maxWidth={false}
-    //   open={open}
-    //   onClose={onClose}
-    //   PaperProps={{
-    //     sx: { maxWidth: 720 },
-    //   }}
-    // >
-    //   <FormProvider methods={methods} onSubmit={onSubmit}>
-    //     <DialogTitle>Update User</DialogTitle>
-    //
-    //     <DialogContent>
-    //       <Grid container spacing={2} rowSpacing={4} sx={{ mt: 1 }}>
-    //         <Grid item xs={6}>
-    //           <RHFTextField name="name" label="Name" required />
-    //         </Grid>
-    //         <Grid item xs={6}>
-    //           <RHFTextField name="email" label="Email" required />
-    //         </Grid>
-    //         <Grid item xs={6}>
-    //           <RHFSelect name="status" label="Status" required>
-    //             {statuses.map((status, index) => (
-    //               <MenuItem key={index} value={status}>
-    //                 <Label
-    //                   variant="soft"
-    //                   color={
-    //                     (status === 'active' && 'success') ||
-    //                     (status === 'pending' && 'warning') ||
-    //                     (status === 'banned' && 'error') ||
-    //                     'default'
-    //                   }
-    //                   sx={{ cursor: 'pointer' }}
-    //                 >
-    //                   {status || 'All'}
-    //                 </Label>
-    //               </MenuItem>
-    //             ))}
-    //           </RHFSelect>
-    //         </Grid>
-    //         <Grid item xs={6}>
-    //           <RHFSelect name="role" label="Role" required>
-    //             {roles.map((status, index) => (
-    //               <MenuItem key={index} value={status}>
-    //                 {status}
-    //               </MenuItem>
-    //             ))}
-    //           </RHFSelect>
-    //         </Grid>
-    //         <Grid item xs={6}>
-    //           <RHFAutocompleteAsyncOnSearch
-    //             initialValues={currentUser.UserRegion.map((region) => ({
-    //               id: region.Region.id,
-    //               name: region.Region.name,
-    //             }))}
-    //             name="regionIds"
-    //             label="Region"
-    //             asyncFn={getRegion}
-    //             multiple
-    //           />
-    //         </Grid>
-    //         <Grid item xs={6}>
-    //           <RHFAutocompleteAsyncOnSearch
-    //             initialValues={currentUser.UserTerritory.map((territory) => ({
-    //               id: territory.Territory.id,
-    //               name: territory.Territory.name,
-    //             }))}
-    //             name="territoryIds"
-    //             label="Territory"
-    //             asyncFn={getTerritory}
-    //             multiple
-    //           />
-    //         </Grid>
-    //       </Grid>
-    //     </DialogContent>
-    //
-    //     <DialogActions>
-    //       <Button variant="outlined" onClick={onClose}>
-    //         Cancel
-    //       </Button>
-    //
-    //       <LoadingButton type="submit" variant="contained" loading={isSubmitting}>
-    //         Update
-    //       </LoadingButton>
-    //     </DialogActions>
-    //   </FormProvider>
-    // </Dialog>
+    <Dialog
+      fullWidth
+      maxWidth={false}
+      open={open}
+      onClose={onClose}
+      PaperProps={{
+        sx: { maxWidth: 720 },
+      }}
+    >
+      <FormProvider methods={methods} onSubmit={onSubmit}>
+        <DialogTitle>Update User</DialogTitle>
+
+        <DialogContent>
+          <Grid container spacing={2} rowSpacing={4} sx={{ mt: 1 }}>
+            <Grid item xs={6}>
+              <RHFTextField name="name" label="Name" required />
+            </Grid>
+            <Grid item xs={6}>
+              <RHFTextField name="email" label="Email" required />
+            </Grid>
+            <Grid item xs={6}>
+              <RHFTextField name="jobTitle" label="Job Title" />
+            </Grid>
+            <Grid item xs={6}>
+              <RHFSelect name="role" label="Role" required>
+                {roles.map((status, index) => (
+                  <MenuItem key={index} value={status}>
+                    {status}
+                  </MenuItem>
+                ))}
+              </RHFSelect>
+            </Grid>
+            <Grid item xs={6}>
+              <RHFSelect name="status" label="Status" required>
+                {statuses.map((status, index) => (
+                  <MenuItem key={index} value={status}>
+                    <Label
+                      variant="soft"
+                      color={
+                        (status === 'active' && 'success') ||
+                        (status === 'pending' && 'warning') ||
+                        (status === 'banned' && 'error') ||
+                        'default'
+                      }
+                      sx={{ cursor: 'pointer' }}
+                    >
+                      {status || 'All'}
+                    </Label>
+                  </MenuItem>
+                ))}
+              </RHFSelect>
+            </Grid>
+          </Grid>
+        </DialogContent>
+
+        <DialogActions>
+          <Button variant="outlined" onClick={onClose}>
+            Cancel
+          </Button>
+
+          <LoadingButton type="submit" variant="contained" loading={isSubmitting}>
+            Update
+          </LoadingButton>
+        </DialogActions>
+      </FormProvider>
+    </Dialog>
   );
 }
