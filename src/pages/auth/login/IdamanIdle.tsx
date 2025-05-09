5aaca8bded611aa533646f57b920ec8144159774
Yudistira Ardi <44466280+arrdix@users.noreply.github.com>
2025-05-09 14:14:44 +0700

    fix(lint): fix linting error

--- src/pages/auth/login/IdamanIdle.tsx
+++ src/pages/auth/login/IdamanIdle.tsx
@@ -1,7 +1,7 @@
 import { LoadingScreen } from '../../../components/loading-screen';
 import { useRouter } from '../../../hooks/use-router';
 import { useAuthContext } from '../../../auth/hooks';
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect } from 'react';
 
 export function IdamanIdle() {
   const router = useRouter();
@@ -11,7 +11,6 @@ export function IdamanIdle() {
   const check = useCallback(() => {
     if (user) {
       router.replace('/');
-    } else {
     }
   }, [user]);
 
